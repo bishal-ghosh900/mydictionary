0f302da67456324cdeb4d391e776f7c97044feaa
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-07-29 02:41:41 +0530

    Fixed the appearing of the loading text in wrong area

--- src/components/Details.js
+++ src/components/Details.js
@@ -7,39 +7,43 @@ function Details({ data, message, loading }) {
 
   return (
     <div className="details">
-      {data?.meanings &&
-        data.meanings.map((v) => {
-          return (
-            <>
-              <h2 className="h2Style">{v.partOfSpeech}</h2>
-              <ul>
-                {v?.definitions?.map((def) => {
-                  return (
-                    <>
-                      <li className="liStyle">
-                        {def.definition ? def.definition : null}
+      {data?.meanings && !loading && (
+        <>
+          <h1 className="h1Style">{data.word}</h1>
+          {data.meanings.map((v) => {
+            return (
+              <>
+                <h2 className="h2Style">{v.partOfSpeech}</h2>
+                <ul>
+                  {v?.definitions?.map((def) => {
+                    return (
+                      <>
+                        <li className="liStyle">
+                          {def.definition ? def.definition : null}
+                          <br />
+                          {def.example && (
+                            <>
+                              <strong>
+                                <em>
+                                  <u>Example</u>:
+                                </em>
+                              </strong>{" "}
+                              {def.example}
+                            </>
+                          )}
+                        </li>
                         <br />
-                        {def.example && (
-                          <>
-                            <strong>
-                              <em>
-                                <u>Example:</u>
-                              </em>
-                            </strong>{" "}
-                            {def.example}
-                          </>
-                        )}
-                      </li>
-                      <br />
-                    </>
-                  );
-                })}
-              </ul>
-            </>
-          );
-        })}
+                      </>
+                    );
+                  })}
+                </ul>
+              </>
+            );
+          })}
+        </>
+      )}
 
-      {message ? <h2 className="h2Style">{message}</h2> : null}
+      {message && !loading ? <h2 className="h2Style">{message}</h2> : null}
       {loading ? (
         <div className="loading">
           {" "}
